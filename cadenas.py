1f89c72c5495d3db0c82dbcb18c7bb2bb551cb2f
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-09 15:33:41 -0400

    uso de comillas simples y dobles

---
+++ cadenas.py
@@ -0,0 +1,12 @@
+# Comillas simples
+fruta = 'manzana'
+
+# Comillas dobles
+color = "Rojo"
+
+cita = 'El maestro dijo :"Estudien mucho"'
+
+libro = "Lei el Libro 'El principito' y me encantó"
+
+print(cita)
+print(libro)
