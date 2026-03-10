54effd726741449653a609887336f7dcc877ad6b
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-10 12:32:22 -0400

    Agregar ejemplos de uso de caracteres especiales en cadenas

---
+++ caracteres_especiales.py
@@ -0,0 +1,19 @@
+# salto de linea \n
+mensaje1 = "Hola\nMundo"
+print(mensaje1)
+
+# Tabulacion \t 
+mensaje2 = "Hola\t\tMundo"
+print(mensaje2)
+
+# Comillas dentro de cadenas
+mensaje3 = "El dijo: \"Hola Mundo\""
+print(mensaje3)
+
+# Barra invertida \\
+mensaje4 = "C:\\Users\\Usuario\\Documentos"
+print(mensaje4)
+
+# Cadenas Crudas (raw strings) r
+mensaje5 = r"C:\Users\Usuario\Documentos"
+print(mensaje5) 
