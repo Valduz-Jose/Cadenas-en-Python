9c371c19d5662281f49d806abeb8ac59804718fe
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-10 14:28:47 -0400

    Agregar ejemplo de multiplicación de cadenas en Python

---
+++ multiplicacion_cadenas.py
@@ -0,0 +1,17 @@
+# el asterisco multiplica el número de veces que se repite la cadena
+# "cadena" * Entero
+
+# crear separador visual
+linea = "=" * 20
+print(linea)
+
+# identacion simple
+nivel = 2
+sangria = "    " * nivel
+print(sangria + "Hola Mundo")
+
+# patrones simples
+print("1"*5)
+
+# error comun
+# print("hola"*2.5) # no se puede multiplicar por un número decimal
